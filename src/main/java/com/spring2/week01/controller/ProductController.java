66f6ac8b9fd928446c3009262d5e1d71581afc1f
Viva <happybird00@naver.com>
2021-03-27 00:40:42 +0900

    tmp2

--- src/main/java/com/spring2/week01/controller/ProductController.java
+++ src/main/java/com/spring2/week01/controller/ProductController.java
@@ -3,8 +3,11 @@ package com.spring2.week01.controller;
 import com.spring2.week01.model.Product;
 import com.spring2.week01.dto.ProductMypriceRequestDto;
 import com.spring2.week01.dto.ProductRequestDto;
+import com.spring2.week01.security.UserDetailsImpl;
 import com.spring2.week01.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.sql.SQLException;
 import java.util.List;
@@ -21,18 +24,22 @@ public class ProductController {
         this.productService = productService;
     }
 
-    // 등록된 전체 상품 목록 조회
+    // 로그인한 회원이 등록한 상품들 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts() throws SQLException {
-        List<Product> products = productService.getProducts();
-        // 응답 보내기
-        return products;
+    public List<Product> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        Long userId = userDetails.getUser().getId();
+        return productService.getProducts(userId);
     }
+
+
 
     // 신규 상품 등록
     @PostMapping("/api/products")
-    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        Product product = productService.createProduct(requestDto);
+    public Product createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 로그인 되어 있는 ID
+        Long userId = userDetails.getUser().getId();
+
+        Product product = productService.createProduct(requestDto, userId);
         // 응답 보내기
         return product;
     }
@@ -42,5 +49,11 @@ public class ProductController {
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
         Product product = productService.updateProduct(id, requestDto);
         return product.getId();
+    }
+    // (관리자용) 등록된 모든 상품 목록 조회
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/api/admin/products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- src/main/java/com/spring2/week01/repository/ProductRepository.java
+++ src/main/java/com/spring2/week01/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.spring2.week01.repository;
 import com.spring2.week01.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository <Product,Long> {
+List<Product> findAllByUserId(Long id);
 }
